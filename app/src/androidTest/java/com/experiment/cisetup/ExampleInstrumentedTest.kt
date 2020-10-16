314686be9a6485cc0d67f39014db6344e671cf15
Tushar Kathuria <tushar.kathuria@compass.com>
2020-10-16 11:46:57 +0530

    Added todo

--- app/src/androidTest/java/com/experiment/cisetup/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/experiment/cisetup/ExampleInstrumentedTest.kt
@@ -14,6 +14,7 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+//TODO: Add more tests
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
