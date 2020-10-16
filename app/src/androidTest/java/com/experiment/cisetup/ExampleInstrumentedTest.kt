f16ff2fcb747631ba1909900d80212f1c9239647
Tushar Kathuria <tushar.kathuria@compass.com>
2020-10-16 15:29:23 +0530

    Screenshot test added

--- app/src/androidTest/java/com/experiment/cisetup/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/experiment/cisetup/ExampleInstrumentedTest.kt
@@ -1,24 +1,40 @@
 package com.experiment.cisetup
 
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.karumi.shot.ScreenshotTest
+import androidx.test.rule.GrantPermissionRule
+import com.karumi.shot.waitForActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleInstrumentedTest : ScreenshotTest {
+    @get:Rule
+    var permissionRule = GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertNotEquals("com.experiment.cisetup", appContext.packageName)
+        assertEquals("com.experiment.cisetup", appContext.packageName)
+    }
+
+    @Test
+    fun screenshotTest() {
+        val mainActivity = ActivityScenario.launch(MainActivity::class.java).waitForActivity()
+        compareScreenshot(mainActivity)
     }
 }
